d7120eb3f8aa8b79e4a52b7ba33a7321333bb37c
Dario Campagna <campagna@esteco.com>
2018-03-12 17:36:43 +0000

    Remove duplication in tests

--- test/bowling_score_test.cpp
+++ test/bowling_score_test.cpp
@@ -1,18 +1,20 @@
 #include "gtest/gtest.h"
 #include "../src/game.h"
 
+void roll(Game* game, int times, int pins) {
+    for(int i = 0; i < times; i++){
+        game->roll(pins);
+    }
+}
+
 TEST(BowlingScoreTest, AllZerosIsZero) {
     Game game;
-    for(int i = 0; i < 20; i++){
-        game.roll(0);
-    }
+    roll(&game, 20, 0);
 	ASSERT_EQ(game.score(), 0);
 }
 
 TEST(BowlingScoreTest, AllOnesIsTwenty) {
     Game game;
-    for(int i = 0; i < 20; i++){
-        game.roll(1);
-    }
+    roll(&game, 20, 1);
     ASSERT_EQ(game.score(), 20);
 }
