b79edc8e2893273b6bc77d3368cd6a02e81bd3cd
Dario Campagna <campagna@esteco.com>
2018-03-12 18:49:42 +0000

    Extract methods in Game class

--- src/game.cpp
+++ src/game.cpp
@@ -6,19 +6,25 @@ void Game::roll(int pins) {
 
 int Game::score() {
     int score = 0;
-    for (int i = 0; i < rolled.size(); i++){
-        if (isSpareFrame(i) && i+2 < rolled.size()-1) {
-            score += spare(i);
-            i++;
-        } else if (rolled[i] == 10 && i+2 < rolled.size()-1) {
-            score += 10 + rolled[i+1] + rolled[i+2];
+    for (int roll = 0; roll < rolled.size(); roll++){
+        if (isSpareFrame(roll) && !isLastFrame(roll)) {
+            score += 10 + spareBonus(roll);
+            roll++;
+        } else if (isStrikeFrame(roll) && !isLastFrame(roll)) {
+            score += 10 + strikeBonus(roll);
         } else {
-            score += rolled[i];
+            score += rolled[roll];
         }
     }
     return score;
 }
 
-int Game::spare(int i) const { return 10 + rolled[i + 2]; }
+int Game::spareBonus(int roll) const { return rolled[roll + 2]; }
 
-bool Game::isSpareFrame(int i) const { return rolled[i] + rolled[i + 1] == 10; }
+int Game::strikeBonus(int roll) const { return rolled[roll+1] + rolled[roll + 2]; }
+
+bool Game::isStrikeFrame(int roll) const { return rolled[roll] == 10; }
+
+bool Game::isLastFrame(int roll) const { return roll + 2 >= rolled.size() - 1; }
+
+bool Game::isSpareFrame(int roll) const { return rolled[roll] + rolled[roll + 1] == 10; }
--- src/game.h
+++ src/game.h
@@ -5,13 +5,17 @@
 
 class Game {
     std::vector<int> rolled;
+    bool isSpareFrame(int roll) const;
+    bool isStrikeFrame(int roll) const;
+    bool isLastFrame(int roll) const;
+    int spareBonus(int roll) const;
+    int strikeBonus(int roll) const;
+
 public:
+
     void roll(int pins);
+
     int score();
-
-    bool isSpareFrame(int i) const;
-
-    int spare(int i) const;
 };
 
 #endif //BOWLINGGAME_GAME_H
